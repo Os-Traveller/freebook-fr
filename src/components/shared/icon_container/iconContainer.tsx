62854b094dc21934f6f1fd38bcdd3a9ee088e181
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2023-04-04 00:17:18 +0600

    border radious fixed in sidebar

--- src/components/shared/icon_container/iconContainer.tsx
+++ src/components/shared/icon_container/iconContainer.tsx
@@ -46,7 +46,7 @@ export function IconContainer({
 
       {/* -------------  profile-page icon container -------------  */}
       {type === "profile-page" && (
-        <p className="cursor-pointer center_y gap-3 justify-center rounded-md hover:bg-white_gray-500 py-2 duration-300  transition-all dark:hover:bg-black-500 text-xs md:text-base">
+        <p className="cursor-pointer center_y gap-3 justify-center hover:bg-white_gray-500 py-2 duration-300 transition-all rounded-br-md rounded-tr-md dark:hover:bg-black-400 text-xs md:text-base">
           {icon}
         </p>
       )}
@@ -54,7 +54,7 @@ export function IconContainer({
       {/* -------------  profile-page-link icon container -------------  */}
       {type === "profile-page-link" && (
         <p
-          className={`flex items-center gap-3 hover:bg-gray-700 hover:text-white dark:hover:bg-black-400 transition-all duration-300 py-2 rounded-br-md rounded-tr-md border-l-4 border-r-4 border-r-transparent px-2 md:px-3 cursor-pointer ${
+          className={`hover:bg-gray-700 hover:text-white dark:hover:bg-black-400 transition-all duration-300 py-2 rounded-br-md rounded-tr-md border-l-4 border-r-4 border-r-transparent px-2 md:px-3 cursor-pointer ${
             activeRoute ? "border-blue-500" : "border-transparent"
           }`}
         >
